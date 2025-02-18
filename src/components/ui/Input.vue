3c4c25054027a813d156570cd83b2bc4aa329c12
김진욱 <jwook0318@gmail.com>
2025-02-18 15:22:27 +0900

    netlify config

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+export interface Props {
+  type: 'text' | 'password'
+}
+withDefaults(defineProps<Props>(), {
+})
+
+const model = defineModel()
+</script>
+
+<template>
+  <div>
+    <input
+      v-model="model"
+      :type="type"
+    >
+  </div>
+</template>
+
+<style scoped>
+
+</style>
