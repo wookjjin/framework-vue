2afa146736ef33d5d2e88ad1cd9d33ce97ce8b6d
JinWook <jwook0318@gmail.com>
2025-03-12 16:44:44 +0900

    feat: add fullcalendar

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,24 +1,17 @@
 <script setup lang="ts">
-const fetchExample = async () => {
-  try {
-    const response = await request.get('/example')
-    console.log(response)
-  }
-  catch (error) {
-    console.error(error)
-  }
-}
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin],
+  initialView: "dayGridMonth",
+  events: [
+    { title: "회의", start: "2025-03-15" },
+    { title: "출장", start: "2025-03-18", end: "2025-03-20" },
+  ],
+});
 </script>
 
 <template>
-  <div>
-    User
-    <button type="button" @click="fetchExample">
-      test
-    </button>
-  </div>
+  <FullCalendar :options="calendarOptions" />
 </template>
-
-<style scoped>
-
-</style>
