be7d92a33b7ab804be3bb6394cb179258a6eca8b
JinWook <jwook0318@gmail.com>
2025-03-13 14:25:08 +0900

    feat: gantt-working-3

--- src/pages/component/gantt/index.vue
+++ src/pages/component/gantt/index.vue
@@ -4,17 +4,26 @@ import { ganttChartConfig, ganttChartData } from '~/composables/gantt'
 
 const chartData = ref({
   ...ganttChartData,
-
 })
-
 const chartConfig = ref(ganttChartConfig)
 const chartKey = ref(0)
-const chartFilter = (e: Event) => {
-  const target = e.target as HTMLInputElement
-  const date = dayjs(target.value)
+const monthPicker = ref({
+  month: new Date().getMonth(),
+  year: new Date().getFullYear(),
+})
+const taskName = ref('')
+const rangePicker = ref()
+const userName = ref('')
+const selectedStatus = ref('')
+const statusOptions = [
+  { value: 'completed', label: 'Completed' },
+  { value: 'delayed', label: 'Delayed' },
+  { value: 'pending', label: 'Pending' },
+]
 
-  const year = date.year()
-  const month = (date.month() + 1).toString().padStart(2, '0')
+const chartFilter = () => {
+  const year = monthPicker.value.year
+  const month = (monthPicker.value.month + 1).toString().padStart(2, '0')
   const lastDay = (y: number, m: number) => {
     return new Date(y, m, 0).getDate()
   }
@@ -27,14 +36,40 @@ const chartFilter = (e: Event) => {
     chartKey.value += 1
   }
 }
+
+const addTask = () => {
+  const newTask = {
+    x: [dayjs(rangePicker.value[0]).format('YYYY-MM-DD'), dayjs(rangePicker.value[1]).format('YYYY-MM-DD')],
+    y: taskName.value,
+    name: userName.value,
+    status: selectedStatus.value,
+  }
+
+  console.log(newTask)
+}
 </script>
 
 <template>
   <div style="height: calc(100% - 120px);">
     <OptiGantt :key="chartKey" :data="chartData" :config="chartConfig" />
   </div>
-  <div>
-    <BaseInput type="month" style="width: 200px;" @change="chartFilter" />
+  <div class="mb-1rem">
+    <VueDatePicker
+      v-model="monthPicker" format="yyyy-MM" locale="ko-KR" style="width: 200px;" month-picker
+      @update:model-value="chartFilter"
+    />
+  </div>
+  <div class="flex gap-2">
+    <BaseInput v-model="taskName" placeholder="Input the Task" type="text" style="width: 200px;" />
+    <VueDatePicker
+      v-model="rangePicker" placeholder="Select Date" format="yyyy-MM-dd" locale="ko-KR" style="width: 400px;" range
+      @update:model-value="chartFilter"
+    />
+    <BaseInput v-model="userName" placeholder="Input the Name" type="text" style="width: 200px;" />
+    <BaseSelect v-model="selectedStatus" :options="statusOptions" style="width: 200px;" />
+    <BaseButton type="button" @click="addTask">
+      Add Task
+    </BaseButton>
   </div>
 </template>
 
