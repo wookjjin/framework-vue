f93d642bab46950e2e8e157fb235ff63d1a998a6
JinWook <jwook0318@gmail.com>
2025-03-18 13:39:30 +0900

    fix: gantt chart

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,5 @@ import type { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void | Promise<void>
 
+export * from './components/tab/tabProps';
 export * from './components/tab/tabSymbol'
-export * from './components/tab/tabProps'
