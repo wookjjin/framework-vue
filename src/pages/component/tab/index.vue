ec3ab8dd2bb2d1cd644a75f2c044d7557b76157a
JinWook <jwook0318@gmail.com>
2025-03-19 10:28:47 +0900

    enhanced: tab component button

--- src/pages/component/tab/index.vue
+++ src/pages/component/tab/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import TabOne from './TabContentOne.vue'
-import TabTwo from './TabContentTwo.vue'
 import TabThree from './TabContentThree.vue'
+import TabTwo from './TabContentTwo.vue'
+
 const tabs = [
   { title: '탭 1', id: 'tab1', content: TabOne },
   { title: '탭 2', id: 'tab2', content: TabTwo },
@@ -11,14 +12,13 @@ const tabs = [
   { title: '탭 6', id: 'tab6', content: '여섯 번째 탭' },
   { title: '탭 7', id: 'tab7', content: '여섯 번째 탭' },
 ]
-
 </script>
 
 <template>
   <div>
     <h2>파스텔 그린 테마 탭 컴포넌트</h2>
 
-    <OrbitTab defaultActiveId="tab1" :tab-items="tabs">
+    <OrbitTab default-active-id="tab1" :tab-items="tabs">
       <OrbitTabPanel v-for="tab in tabs" :id="tab.id" :key="tab.id">
         <template v-if="typeof tab.content === 'object'">
           <component :is="tab.content" />
